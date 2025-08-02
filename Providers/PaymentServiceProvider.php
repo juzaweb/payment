8b161de93f00f4c92bb63af05bc773d14b23f880
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-08-02 12:05:07 +0700

    feat: Begin a project

---
+++ Providers/PaymentServiceProvider.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Juzaweb\Modules\Payment\Providers;
+
+use Juzaweb\Core\Providers\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/config.php' => config_path('payment.php'),
+        ], 'config');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'payment');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    protected function registerTranslations(): void
+    {
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'payment');
+        $this->loadJsonTranslationsFrom(__DIR__ . '/../resources/lang', 'payment');
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    protected function registerViews(): void
+    {
+        $viewPath = resource_path('views/modules/payment');
+
+        $sourcePath = __DIR__ . '/../resources/views';
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ], ['views', 'payment-module-views']);
+
+        $this->loadViewsFrom($sourcePath, 'payment');
+    }
+}
