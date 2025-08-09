81dad5b555be8b18fc89ed9dbc0a9a405a970d23
Thế Anh Đặng <dangtheanh16@gmail.com>
2025-08-09 14:54:02 +0700

    Adds Payos payment gateway method
    Adds the Payos payment gateway integration.
    
    This allows the application to process payments through the Payos service.
    It includes methods for purchasing, completing transactions, and handling webhooks.

---
+++ Methods/Payos.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * JUZAWEB CMS - Laravel CMS for Your Project
+ *
+ * @package    juzaweb/cms
+ * @author     The Anh Dang
+ * @link       https://cms.juzaweb.com
+ * @license    GNU V2
+ */
+
+namespace Juzaweb\Modules\Payment\Methods;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Juzaweb\Modules\Payment\Contracts\PaymentGatewayInterface;
+use Juzaweb\Modules\Payment\Exceptions\PaymentException;
+use Juzaweb\Modules\Payment\Services\CompleteResult;
+use Juzaweb\Modules\Payment\Services\PurchaseResult;
+use Omnipay\Common\GatewayInterface;
+use Omnipay\Omnipay;
+
+class Payos implements PaymentGatewayInterface
+{
+    public function __construct(protected array $config)
+    {
+    }
+
+    public function purchase(array $params): PurchaseResult
+    {
+        $response = $this->createGateway()->purchase($params)->send();
+
+        if (! in_array($response->getCode(), [200, 201])) {
+            throw new PaymentException(
+                __('Payment gateway error: :message', ['message' => $response->getMessage()])
+            );
+        }
+
+        return PurchaseResult::make(
+            $response->getTransactionReference(),
+            $response->getRedirectUrl(),
+            $response->getData()
+        )->setSuccessful($response->isSuccessful() && !$response->isRedirect());
+    }
+
+    public function complete(array $params): CompleteResult
+    {
+        $response = $this->createGateway()->completePurchase($params)->send();
+
+        return CompleteResult::make(
+            $response->getTransactionReference(),
+            $response->isSuccessful(),
+            $response->getData()
+        );
+    }
+
+    public function handleWebhook(Request $request): CompleteResult
+    {
+        // TODO: Implement handleWebhook() method.
+    }
+
+    protected function createGateway(): GatewayInterface
+    {
+        $gateway = Omnipay::create('Payos');
+        $gateway->initialize(Arr::except($this->config, ['sandbox', 'token']));
+        if (isset($this->config['sandbox']) && $this->config['sandbox']) {
+            $gateway->setTestMode(true);
+        }
+        return $gateway;
+    }
+}
